3445518a3a6668cd7a1af137cfe511c97a6512fd
Silke Bonnen <silke.bonnen@gmail.com>
2022-09-14 14:15:42 +0200

    url method and testing done

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -75,26 +75,63 @@ public class RegExprTests
     }
 
     [Fact]
-    public void Urls_should_return_if_any_url_and_title()
+    public void Urls_should_return_url_and_title_one_url()
     {
-        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
-    
+        // Given
+        //var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+        var testString = "<a href=\"https://www.w3schools.com/\"  title=\"Hello\">Visit W3Schools.com!</a>";
+
         // When
         var result = RegExpr.Urls(testString);
 
         // Then
-        result.Should().BeEquivalentTo(new List<(string, string) >{("https://en.wikipedia.org/wiki/Theoretical_computer_science", "Theoretical computer science") , ("https://en.wikipedia.org/wiki/Formal_language","Formal language)") , ("https://en.wikipedia.org/wiki/Character_(computing)","Character (computing)"), ("https://en.wikipedia.org/wiki/Pattern_matching","Pattern matching"), ("https://en.wikipedia.org/wiki/String_searching_algorithm", "String searching algorithm"), ("https://en.wikipedia.org/wiki/String_(computer_science)", "String (computer science)")});
+        //result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science") , (new Uri("https://en.wikipedia.org/wiki/Formal_language"),"Formal language)") , (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"),"Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"),"Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")});
+        result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://www.w3schools.com/"), "Hello")});
 
     }
 
-     [Fact]
-    public void Urls_should_return_innertext_if_no_url_present()
+    [Fact]
+    public void Urls_should_return_url_and_innertext_one_url()
     {
         // Given
-    
+        //var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+        var testString = "<a href=\"https://www.w3schools.com/\" >Visit W3Schools.com!</a>";
+
         // When
-    
+        var result = RegExpr.Urls(testString);
+
         // Then
+        //result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science") , (new Uri("https://en.wikipedia.org/wiki/Formal_language"),"Formal language)") , (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"),"Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"),"Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")});
+        result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://www.w3schools.com/"), "Visit W3Schools.com!")});
+
+    }
+
+    [Fact]
+    public void Urls_should_return_url_and_title_multiple_urls()
+    {
+        // Given
+        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+
+        // When
+        var result = RegExpr.Urls(testString);
+
+        // Then
+        result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "Theoretical computer science") , (new Uri("https://en.wikipedia.org/wiki/Formal_language"),"Formal language") , (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"),"Character (computing)"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"),"Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")});
+
+    }
+
+    [Fact]
+    public void Urls_should_return_url_and_title_or_innertext_multiple_urls()
+    {
+        // Given
+        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" >theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+
+        // When
+        var result = RegExpr.Urls(testString);
+
+        // Then
+        result.Should().BeEquivalentTo(new List<(Uri, string) >{(new Uri("https://en.wikipedia.org/wiki/Theoretical_computer_science"), "theoretical computer science") , (new Uri("https://en.wikipedia.org/wiki/Formal_language"),"Formal language") , (new Uri("https://en.wikipedia.org/wiki/Character_(computing)"),"characters"), (new Uri("https://en.wikipedia.org/wiki/Pattern_matching"),"Pattern matching"), (new Uri("https://en.wikipedia.org/wiki/String_searching_algorithm"), "String searching algorithm"), (new Uri("https://en.wikipedia.org/wiki/String_(computer_science)"), "String (computer science)")});
+
     }
 
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -4,62 +4,75 @@ using System.Text.RegularExpressions;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) {
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
 
-        foreach(var line in lines){
-            var matches =  Regex.Matches(line,@"\w+");
+        foreach (var line in lines)
+        {
+            var matches = Regex.Matches(line, @"\w+");
 
-            foreach(Match word in matches){
+            foreach (Match word in matches)
+            {
                 yield return word.Value;
             }
         }
 
-        
-    }       
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) {
-            var matches = Regex.Matches(resolutions, @"((?<width>[\d]{1,4}).*?(?<height>[\d]{1,4}).*?)");
+    }
 
-            foreach(Match match in matches){
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions)
+    {
+        var matches = Regex.Matches(resolutions, @"((?<width>[\d]{1,4}).*?(?<height>[\d]{1,4}).*?)");
 
-                if(match.Success){
+        foreach (Match match in matches)
+        {
+
+            if (match.Success)
+            {
                 string width = match.Groups["width"].Value;
                 string height = match.Groups["height"].Value;
 
-                yield return((int.Parse(width), int.Parse(height)));
-                
+                yield return ((int.Parse(width), int.Parse(height)));
+
             }
         }
 
-    
-     }
 
-    public static IEnumerable<string> InnerText(string html, string tag) 
+    }
+
+    public static IEnumerable<string> InnerText(string html, string tag)
     {
-     var matches = Regex.Matches(html, @"(<" + tag + @"{1})[^>]*(>{1})(?<innertext>[\w\s]*)");
+        var matches = Regex.Matches(html, @"(<" + tag + @"{1})[^>]*(>{1})(?<innertext>[\w\s]*)");
 
-            foreach(Match match in matches){
+        foreach (Match match in matches)
+        {
 
-                if(match.Success){
+            if (match.Success)
+            {
                 string innertext = match.Groups["innertext"].Value;
-                
-                yield return(innertext);
+
+                yield return (innertext);
             }
         }
     }
 
-    public static IEnumerable<(string url, string title)> Urls (string html) 
+    public static IEnumerable<(Uri url, string title)> Urls(string html)
     {
-     var matches = Regex.Matches(html, @"((?<width>[\d]{1,4}).*?(?<height>[\d]{1,4}).*?)");
+        var matches = Regex.Matches(html, @".*?href=""(?<url>\S*)"".*?(title=""(?<title>.*?)"".*?)?>(?<innerText>.*?)<");
 
-            foreach(Match match in matches){
+        foreach (Match match in matches)
+        {
 
-                if(match.Success){
-                string width = match.Groups["width"].Value;
-                string height = match.Groups["height"].Value;
+            if (match.Success)
+            {
+                string stringUrl = match.Groups["url"].Value;
+                Uri url = new Uri(stringUrl);
+                string innerText = match.Groups["innerText"].Value;
+                string title = match.Groups["title"].Value;
 
-                yield return((int.Parse(width), int.Parse(height)));
-                
+                if (title != "") yield return ((url, title));
+                else yield return ((url, innerText));
+
             }
         }
     }
