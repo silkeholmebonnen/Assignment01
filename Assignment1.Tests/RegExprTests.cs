8030455da767b7fcbad87046eb72179788fc64c9
monicahardt <monha@itu.dk>
2022-09-14 12:30:48 +0200

    working on urls

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -17,5 +17,84 @@ public class RegExprTests
         result.Should().BeEquivalentTo(new List<string>{"hello", "Monica", "Silke", "turned", "23", "happy", "birthday"});
     }
 
+            [Fact]
+    public void Resolution_should_return_tuples_of_input_resolutions()
+    {
+        // Given
+        var testString = "1920x1080";
+    
+        // When
+        var result = RegExpr.Resolution(testString);
+
+
+        // Then
+        result.Should().BeEquivalentTo(new List<(int, int)>{(1920, 1080)});
+    }
+
+    [Fact]
+    public void Resolution_should_return_tuples_of_input_resolutions_multiple_res()
+    {
+        // Given
+        var testString = "1920x1080, 860x220, 720x300" ;
+    
+        // When
+        var result = RegExpr.Resolution(testString);
+
+
+        // Then
+        result.Should().BeEquivalentTo(new List<(int, int)>{(1920, 1080), (860, 220), (720, 300)});
+    }
+    [Fact]
+    public void Innertext_should_find_innertext_of_specific_tag_a()
+    {
+        // Given
+        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a>" ;
+    
+        // When
+        var result = RegExpr.InnerText(testString, "a");
+
+
+        // Then
+        result.Should().BeEquivalentTo(new List<string>{"theoretical computer science"});
+
+    }
+
+    [Fact]
+    public void Innertext_should_find_innertext_of_specific_tag_b()
+    {
+        // Given
+        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+    
+        // When
+        var result = RegExpr.InnerText(testString, "b");
+
+
+        // Then
+        result.Should().BeEquivalentTo(new List<string>{"regular expression", "regex", "regexp", "rational expression"});
+
+    }
+
+    [Fact]
+    public void Urls_should_return_if_any_url_and_title()
+    {
+        var testString = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p></div>";
+    
+        // When
+        var result = RegExpr.Urls(testString);
+
+        // Then
+        result.Should().BeEquivalentTo(new List<(string, string) >{("https://en.wikipedia.org/wiki/Theoretical_computer_science", "Theoretical computer science") , ("https://en.wikipedia.org/wiki/Formal_language","Formal language)") , ("https://en.wikipedia.org/wiki/Character_(computing)","Character (computing)"), ("https://en.wikipedia.org/wiki/Pattern_matching","Pattern matching"), ("https://en.wikipedia.org/wiki/String_searching_algorithm", "String searching algorithm"), ("https://en.wikipedia.org/wiki/String_(computer_science)", "String (computer science)")});
+
+    }
+
+     [Fact]
+    public void Urls_should_return_innertext_if_no_url_present()
+    {
+        // Given
+    
+        // When
+    
+        // Then
+    }
 
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -15,18 +15,54 @@ public static class RegExpr
         }
 
         
-       
-        
     }       
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions) 
-    {
-        throw new NotImplementedException();
-    }
+    public static IEnumerable<(int width, int height)> Resolution(string resolutions) {
+            var matches = Regex.Matches(resolutions, @"((?<width>[\d]{1,4}).*?(?<height>[\d]{1,4}).*?)");
 
+            foreach(Match match in matches){
+
+                if(match.Success){
+                string width = match.Groups["width"].Value;
+                string height = match.Groups["height"].Value;
+
+                yield return((int.Parse(width), int.Parse(height)));
+                
+            }
+        }
+
+    
+     }
 
     public static IEnumerable<string> InnerText(string html, string tag) 
     {
-        throw new NotImplementedException();
+     var matches = Regex.Matches(html, @"(<" + tag + @"{1})[^>]*(>{1})(?<innertext>[\w\s]*)");
+
+            foreach(Match match in matches){
+
+                if(match.Success){
+                string innertext = match.Groups["innertext"].Value;
+                
+                yield return(innertext);
+            }
+        }
     }
+
+    public static IEnumerable<(string url, string title)> Urls (string html) 
+    {
+     var matches = Regex.Matches(html, @"((?<width>[\d]{1,4}).*?(?<height>[\d]{1,4}).*?)");
+
+            foreach(Match match in matches){
+
+                if(match.Success){
+                string width = match.Groups["width"].Value;
+                string height = match.Groups["height"].Value;
+
+                yield return((int.Parse(width), int.Parse(height)));
+                
+            }
+        }
+    }
+
+
 }
